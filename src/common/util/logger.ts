0b107cfd0269f488a3716d85cd3807bf9a16de24
Marcony Neves <marconyneves@gmail.com>
2022-09-25 15:20:01 +0200

    Initial commit

---
+++ src/common/util/logger.ts
@@ -0,0 +1,12 @@
+export enum LogTypes {
+  ERROR = "ERROR",
+  INFO = "INFO",
+  DEBUG = "DEBUG",
+  VERBOSE = "VERBOSE",
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function logger(this: any, lt: LogTypes = LogTypes.INFO, ...args: any) {
+  // eslint-disable-next-line no-console
+  console.log.apply(this, [`${lt}:`, ...args]);
+}
